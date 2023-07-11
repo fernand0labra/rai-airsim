aa42f483f515aa8f7bdfa95b25bfe852a5ee04a5
fernand0labra <fernandolabracaso@gmail.com>
2023-07-11 15:46:19 +0200

    Week 28 - Tuesday 11/07/2023

--- workspace/src/simulation/shell/installation.sh
+++ workspace/src/simulation/shell/installation.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# Unreal Engine
+##############################################################
+#                        Unreal Engine                       #
+##############################################################
+
 git clone -b 4.27 https://github.com/EpicGames/UnrealEngine.git
 chmod u+x Setup.sh; ./Setup.sh
 chmod u+x GenerateProjectFiles.sh; ./GenerateProjectFiles.sh
 make
 
-./Engine/Binaries/Linux/UE4Editor
+./UnrealEngine-4.27/Engine/Binaries/Linux/UE4Editor
 
 
-# AirSim Microsoft Fork
+##############################################################
+#                    AirSim Microsoft Fork                   #
+##############################################################
+
 export AIRSIM_PATH_1=/media/student/New\\ 
-export AIRSIM_PATH_2=\ Volume/fernand0labra/ros-airsim-compatibility/workspace/src/AirSim
+export AIRSIM_PATH_2=\ Volume/AirSim
 export AIRSIM_PATH=$AIRSIM_PATH_1$AIRSIM_PATH_2 # Set the target destination.
 
 cd $AIRSIM_PATH
@@ -23,22 +29,44 @@ cd AirSim
 chmod u+x setup.sh; ./setup.sh 
 chmod u+x build.sh; ./build.sh
 
-./Engine/Binaries/Linux/UE4Editor $AIRSIM_PATH/Unreal/Environments/Blocks/Blocks.uproject
+./UnrealEngine-4.27/Engine/Binaries/Linux/UE4Editor $AIRSIM_PATH/Unreal/Environments/Blocks/Blocks.uproject
+
+#------------------------------------------------------------#
+#             Python API (AirSim/PythonClient)               #
+#------------------------------------------------------------#
+
+pip install msgpack-rpc-python
+cd AirSim/PythonClient/car; python3 hello_car.py
+cd AirSim/PythonClient/multirotor; python3 hello_drone.py  
+
+#------------------------------------------------------------#
+#              Airsim ROS Wrapper (AirSim/ros)               #
+#------------------------------------------------------------#
+
+sudo apt-get install ros-noetic-tf2-sensor-msgs ros-noetic-tf2-geometry-msgs ros-noetic-mavros*
+pip install "git+https://github.com/catkin/catkin_tools.git#egg=catkin_tools"  # Ubuntu 20.02
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+cd AirSim/ros; catkin build;
 
 
-# Unreal Airsim ROS Package
+##############################################################
+#                   Unreal Airsim ROS Package                #
+##############################################################
+
+# Mesh Position
 sudo apt-get install python3-wstool python3-catkin-tools ros-noetic-cmake-modules ros-noetic-tf2-sensor-msgs
-cd ..
-
 git clone https://github.com/ethz-asl/unreal_airsim.git
 
-# wstool init . .rosinstall  # If non existant
+wstool init . .rosinstall  # If non existant
 wstool update
 
 cd ~/ros-airsim-compatibility/workspace/src/unreal_airsim/
 echo "set(AIRSIM_ROOT $AIRSIM_PATH)" > ./AirsimPath.txt
 catkin build unreal_airsim
-source ../devel/setup.bash
+source ./fernand0labra/ros-airsim-compatibility/workspace/devel/setup.bash
 
 # With specific configuration
 # roslaunch unreal_airsim parse_config_to_airsim.launch source:=path/to/my_settings.yaml
