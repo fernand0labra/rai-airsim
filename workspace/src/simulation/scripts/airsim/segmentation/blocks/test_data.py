06f26a9b861caa6b9c609bdfd6558e4c14c61fa4
fernand0labra <fernandolabracaso@gmail.com>
2023-08-01 16:04:49 +0200

    Week 31 - Tuesday 01/08/2023

--- workspace/src/simulation/scripts/airsim/segmentation/blocks/test_data.py
+++ workspace/src/simulation/scripts/airsim/segmentation/blocks/test_data.py
@@ -1,23 +1,60 @@
 import rospy
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 from mpl_toolkits.mplot3d import Axes3D
-from unreal_airsim.msg import Airsim
+from simulation.msg import Airsim
 
+import os
+import cv2
+import signal
 import numpy as np
 import pandas as pd
 
+
+FOLDER = '/home/fernand0labra/ros-airsim/docs/segmentation'
+N_MSGS = 0
+
+def generate_plot_video_opencv(signum, frame):
+    global N_MSGS, FOLDER
+    
+    os.chdir(FOLDER)
+    video = cv2.VideoWriter('airsim_plot.mp4', 0x7634706d, 3.0, (1200, 600))
+
+    for idx in range(N_MSGS+1):
+        img_path = 'point_plot' + str(idx) + '.png'
+        video.write(cv2.imread(img_path))
+        os.remove(img_path)
+        
+    video.release()
 
 def callback(airsim_msg):
     n_points = len(airsim_msg.mesh_ids.data)
 
     point_list = np.empty((3, n_points), dtype=np.float32)  # [x, y, z]
-    color_list = np.empty((n_points, 4), dtype=np.float32)    # [r, g, b, alpha]
+    color_list = np.empty((n_points, 4), dtype=np.float32)  # [r, g, b, alpha]
 
-    # plt.imshow(np.array(bytearray(airsim_msg.seg_img.data), dtype=np.uint8).reshape(airsim_msg.seg_img.height, airsim_msg.seg_img.width, 3))
-    # plt.show()
+    global N_MSGS
+    img = np.array(bytearray(airsim_msg.seg_img.data), dtype=np.uint8).reshape(airsim_msg.seg_img.height, airsim_msg.seg_img.width, 3)
+    img = img[:][:]/255
 
-    fig = plt.figure()
-    ax = Axes3D(fig)
+    fig = plt.figure(figsize=(12, 6))
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122, projection='3d')
+
+    ax1.imshow(img)
+    ax1.axis('off')
+    ax1.set_title('Segmentation Image')
+
+
+    ax2.set_xlim(-60, 60)
+    ax2.set_ylim(-60, 60)
+    ax2.set_zlim(-5, 8)
+
+    ax2.set_xlabel('X')
+    ax2.set_ylabel('Y')
+    ax2.set_zlabel('Z')
+
+    ax2.set_title('3D Scatter Plot')
 
     for idx in range(n_points):
         point = airsim_msg.mesh_location.poses[idx].position
@@ -28,20 +65,26 @@ def callback(airsim_msg):
 
         global color_checks
         color = color_checks.iloc[airsim_msg.mesh_ids.data[idx]]
-        print(color[1:3])
-        print(color[1:3]/255)
-        color_list[idx] = [color.iloc[1]/255, color.iloc[1]/255, color.iloc[3]/255, 1]
+        color_list[idx] = [color.iloc[1]/255, color.iloc[2]/255, color.iloc[3]/255, 1]
     
     vehicle_position = airsim_msg.odom_gt.pose.pose.position
 
-    ax.scatter(point_list[0], point_list[1], point_list[2], c=color_list, marker='o')
-    ax.scatter(vehicle_position.x, vehicle_position.y, vehicle_position.z, marker='^')
-    plt.show()
+    ax2.scatter(point_list[0], point_list[1], point_list[2], c=color_list, marker='o')
+    ax2.scatter(vehicle_position.x, vehicle_position.y, vehicle_position.z, marker='^')
 
-color_checks_path = '/media/student/New Volume/fernand0labra/ros-airsim-compatibility/docs/segmentation/seg_rgbs.txt'
+    plt.tight_layout()
+
+    os.chdir(FOLDER)
+    plt.savefig('point_plot' + str(N_MSGS) + '.png')
+    N_MSGS += 1
+    exit()
+
+signal.signal(signal.SIGTSTP, generate_plot_video_opencv)
+
+color_checks_path = '/home/fernand0labra/ros-airsim/docs/segmentation/seg_rgbs.txt'
 color_checks = pd.read_csv(color_checks_path, sep = ' ', header=None)
 
 rospy.init_node('test_data', anonymous=True)
-rospy.Subscriber('/airsim_drone/segmentation', Airsim, callback, queue_size=1)
+rospy.Subscriber('/airsim_drone/segmentation', Airsim, callback, queue_size=10)
 
 rospy.spin()
